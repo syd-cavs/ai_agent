b79f57022acd887f033a05ff19588084fbfdd389
syd-cavs <sydney.cavallaro@gmail.com>
2025-08-27 15:37:29 -0400

    B: Updated main.py to use command/terminal inputs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,10 +7,15 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
+def run_prompt():
+    response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=sys.argv[1]
+    )
+    if not sys.argv[1]:
+        print("Error: no prompt found")
+        sys.exit(1)
+    print(response.text)
+    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
 
-print(response.text)
-print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+run_prompt()
